8c21e2a98f89e9dc8b1aa72e8fefb21d7e5c3964
Gabriel Bromley <bromlgab@gmail.com>
2025-06-05 11:10:07 -0600

    Create Sqlalchemy models for Station, Sources (#7)
    * added station model
    
    * added source to deal with different DOT datasets
    
    * Ruff formatting
    
    * dont check for Ruff F821

---
+++ database/models/source.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+
+
+class Source(Base):
+    __tablename__ = 'sources'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[str] = mapped_column(String, nullable=False)
+    api_endpoint: Mapped[str] = mapped_column(String, nullable=False)
+
+    stations: Mapped[List['Station']] = relationship('Station', back_populates='source')
---
+++ database/models/station.py
@@ -0,0 +1,28 @@
+from typing import List
+
+from sqlalchemy import REAL, BigInteger, ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+
+
+class Station(Base):
+    __tablename__ = 'stations'
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    source_id: Mapped[int] = mapped_column(ForeignKey('sources.id'))
+    name: Mapped[str] = mapped_column(String)
+    latitude: Mapped[float] = mapped_column(REAL, nullable=False)
+    longitude: Mapped[float] = mapped_column(REAL, nullable=False)
+    elevation: Mapped[float] = mapped_column(REAL, nullable=True)
+    road_name: Mapped[str] = mapped_column(String, nullable=True)
+    road_direction: Mapped[str] = mapped_column(String, nullable=True)
+
+    observations: Mapped[List['Observation']] = relationship(
+        'Observation', back_populates='station'
+    )
+    station_variables: Mapped[List['StationVariable']] = relationship(
+        'StationVariable', back_populates='station'
+    )
+
+    source: Mapped['Source'] = relationship('Source', back_populates='stations')
