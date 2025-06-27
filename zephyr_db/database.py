d526ef800590bcef439ac2c1fe3ec4dbc543d1b2
gbromley <bromlgab@gmail.com>
2025-06-27 10:39:37 -0600

    changing default environment names

--- zephyr_db/database.py
+++ zephyr_db/database.py
@@ -8,11 +8,11 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv()
 
 
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST', 'localhost')
-DB_PORT = os.getenv('DB_PORT', '5432')
-DB_NAME = os.getenv('DB_NAME', 'zephyr_transit')
+DB_USER = os.getenv('POSTGRES_USER')
+DB_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+DB_HOST = os.getenv('POSTGRES_HOST', 'localhost')
+DB_PORT = os.getenv('POSTGRES_PORT', '5432')
+DB_NAME = os.getenv('POSTGRES_NAME', 'zephyr_transit')
 
 DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
