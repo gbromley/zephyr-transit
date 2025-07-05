b09902ce0628fc330ffb7af102bc043d9ea1b6cc
gbromley <bromlgab@gmail.com>
2025-07-05 11:01:44 -0600

    formatting

--- zephyr_db/seed/variables.py
+++ zephyr_db/seed/variables.py
@@ -5,32 +5,29 @@ from zephyr_db.models import Variable
 
 from .utils import seed_table
 
-VARIABLES = [{'name': 'wind speed', 'unit': 'wind speed'},
-             {'name': 'wind direction', 'unit': 'wind dir'}]
-
-
+VARIABLES = [
+    {'name': 'wind speed', 'unit': 'wind speed'},
+    {'name': 'wind direction', 'unit': 'wind dir'},
+]
 
 
 def seed_variables() -> None:
     """Populate database with measurement variables."""
-    units_df = pl.read_database("SELECT * FROM units", engine)
+    units_df = pl.read_database('SELECT * FROM units', engine)
 
     variables = []
 
     for var in VARIABLES:
-
-        if "unit" not in var:
+        if 'unit' not in var:
             continue
 
-
-        unit_id = units_df.filter(pl.col("name") == var["unit"]).select("id").item()
-
+        unit_id = units_df.filter(pl.col('name') == var['unit']).select('id').item()
 
         variables.append(
             {
-                "name": var['name'],
-                "unit": var['unit'],
-                "unit_id": unit_id,
+                'name': var['name'],
+                'unit': var['unit'],
+                'unit_id': unit_id,
             }
         )
     seed_table(Variable, variables)
